89f7adc489b49aa64b63c2845e38f51d0aea1230
Kuzi <154668242+kuzi23@users.noreply.github.com>
2025-05-17 21:12:06 +0200

    Update settings.py

--- site79110/settings.py
+++ site79110/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2=3^_j$g-qwm3*_c@@w8^g(y2-ttg0^fon8@tv5=j-*gy4g#0f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
